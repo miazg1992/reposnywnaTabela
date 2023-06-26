68f10e599d3cd96cb37c234478f337fc197d40cb
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2023-06-26 10:54:52 +0200

    basic webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Photo from './img1.jpg';
+import './components/main.scss';
+
+
+function addImage(img) {
+    const image = document.createElement("img");
+    image.src = img;
+    document.body.appendChild(image)
+}
+
+addImage(Photo)
