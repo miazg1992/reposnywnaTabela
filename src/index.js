c357a560497fb1577fae82a347d2ea1d29b8489c
Wioletta Miazga <wioletta.miazga1992@gmail.com>
2023-06-30 12:06:48 +0200

    table and list responsive

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
-import Photo from './assets/img1.jpg';
-import './components/main.scss';
+const values = [
+    { name: "Ananas", calories: 33, fat: 0, carbs: 11.8 },
+    { name: "Jabłko", calories: 57, fat: 0, carbs: 11.8 },
+    { name: "Pomarańcza", calories: 51, fat: 0, carbs: 11.8 },
+    { name: "Wiśnia", calories: 67, fat: 0, carbs: 14.6 },
+]
 
+const valuesContainer = document.querySelector('.app__values');
 
-function addImage(img) {
-    const image = document.createElement("img");
-    image.src = img;
-    document.body.appendChild(image)
+const desktopViewport = window.matchMedia("screen and (min-width: 500px)");
+
+const drawValues = (isDesktop) => {
+    if (isDesktop) {
+        drawDesktopValues();
+    } else {
+        drawMobileValues();
+    }
 }
 
-addImage(Photo)
+const drawMobileValues = () => {
+    valuesContainer.innerHTML = "";
+    let list = document.createElement("ul");
 
+    values.forEach(value => {
+        let element = document.createElement("li");
+        let name = document.createElement("div");
+        name.innerHTML = `<strong>Nazwa: </strong>${value.name}`;
+        let calories = document.createElement("div");
+        calories.innerHTML = `<strong>Kalorie: </strong>${value.calories}`;
+        let fat = document.createElement("div");
+        fat.innerHTML = `<strong>Tłuszcz: </strong>${value.fat}`;
+        let carbs = document.createElement("div");
+        carbs.innerHTML = `<strong>Węglowodany: </strong>${value.carbs}`;
+
+        element.appendChild(name);
+        element.appendChild(calories);
+        element.appendChild(fat);
+        element.appendChild(carbs);
+
+        list.appendChild(element);
+
+    })
+
+    valuesContainer.appendChild(list);
+}
+
+
+const drawDesktopValues = () => {
+    valuesContainer.innerHTML = '';
+
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    thead.innerHTML = "<tr><th>Nazwa</th><th>Kalorie</th><th>Tłuszcz</th><th>Węglowodany</th></tr>"
+
+    let tbody = document.createElement("tbody");
+
+    values.forEach(value => {
+        let row = document.createElement("tr");
+        row.innerHTML = `<td>${value.name}</td><td>${value.calories}</td><td>${value.fat}</td><td>${value.carbs}</td>`;
+        tbody.appendChild(row);
+    })
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    valuesContainer.appendChild(table);
+}
+
+drawValues(desktopViewport.matches);
+
+desktopViewport.addEventListener("change", e => {
+    drawValues(e.matches);
+});
